56f2a10165e29059e06debe15ebf4c2a05bcaa0a
Khenziforland <khenziforland@gmail.com>
2024-06-02 17:20:35 +0700

    integration dashboard

---
+++ app/Http/Controllers/User/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Http\Controllers\Controller;
+use App\Models\Movie;
+
+class DashboardController extends Controller
+{
+    public function index() {
+        $featuredMovies = Movie::whereIsFeatured(true)->get();
+        $movies = Movie::all();
+        
+        return inertia('User/Dashboard/index', [
+            'featuredMovies' => $featuredMovies,
+            'movies' => $movies,
+        ]);
+    }
+    
+}
